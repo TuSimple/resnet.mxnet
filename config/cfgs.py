26d969fcf43be10ee318d6bacff94d8b013505e9
huangzehao <huangzehao@outlook.com>
2017-08-22 11:34:39 +0800

    init

---
+++ config/cfgs.py
@@ -0,0 +1,43 @@
+mxnet_path = '../incubator-mxnet/python/'
+gpu_list = [0, 1, 2, 3]
+dataset = "imagenet"
+model_prefix = "resnext50"
+network = "resnext"
+depth = 50
+model_load_prefix = model_prefix
+model_load_epoch = 100
+retrain = False
+
+# data
+data_dir = '/home/hzh/imagenet_data_new/'
+batch_size = 128
+kv_store = 'local'
+
+# optimizer
+lr = 0.1
+wd = 0.0001
+momentum = 0.9
+if dataset == "imagenet":
+    lr_step = [30, 60, 90]
+else:
+    lr_step = [120, 160, 240]
+lr_factor = 0.1
+begin_epoch = 0
+num_epoch = 100
+frequent = 50
+# network config
+if dataset == "imagenet":
+    num_classes = 1000
+    units_dict = {"18": [2, 2, 2, 2],
+                  "34": [3, 4, 6, 3],
+                  "50": [3, 4, 6, 3],
+                  "101": [3, 4, 23, 3],
+                  "152": [3, 8, 36, 3]}
+    units = units_dict[str(depth)]
+    if depth >= 50:
+        filter_list = [64, 256, 512, 1024, 2048]
+        bottle_neck = True
+    else:
+        filter_list = [64, 64, 128, 256, 512]
+        bottle_neck = False
+    num_stage = 4
---
+++ symbol/__init__.py
@@ -0,0 +1,5 @@
+from resnet import resnet
+from resnext import resnext
+from vgg16 import vgg16
+from mobilenet import mobilenet
+from shufflenet import shufflenet
