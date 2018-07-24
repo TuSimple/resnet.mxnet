44feeaf65745c4a88d2528e9897f903d791b9c1c
XiaotaoChen <chenxiaotao1234@gmail.com>
2018-07-24 19:35:01 +0800

    configuration for resnext152(64*4d) with dist_sync_device

--- config/edict_config.py
+++ config/edict_config.py
@@ -6,21 +6,21 @@ config = edict()
 config.mxnet_path = '../mxnet/python/'
 config.gpu_list = [0, 1, 2, 3, 4, 5, 6, 7]
 config.dataset = "imagenet"
-config.model_prefix = "resnext50"
-config.network = "resnext"
-config.depth = 50
+config.model_prefix = "resnext152"
+config.network = "resnext_cyt"
+config.depth = 152
 config.model_load_prefix = config.model_prefix
 config.model_load_epoch = 0
 config.retrain = False
 
 # data
 config.data_dir = '/data/ILSVRC2012'
-config.batch_size = 32
+config.batch_size = 16
 config.batch_size *= len(config.gpu_list)
-config.kv_store = 'dist_sync'
+config.kv_store = 'dist_sync_device'
 
 # optimizer
-config.lr = 0.4
+config.lr = 0.2
 config.wd = 0.0001
 config.momentum = 0.9
 if config.dataset == "imagenet":
@@ -37,8 +37,8 @@ config.warmup_lr = 0.1
 config.warm_epoch = 5
 # set image_shape for io and network
 config.image_shape = [3, 224, 224]
-config.benchmark = True
-config.num_group = 32
+config.benchmark = False
+config.num_group = 64
 config.data_type = 'float32'
 
 
